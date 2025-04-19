e90bb80f67547658e3cd6efebcf493f1d836f6d1
michael5434 <andreacolaianni04@gmail.com>
2025-04-19 17:47:42 +0200

    New changes with button

--- index/index.js
+++ index/index.js
@@ -1,19 +1,34 @@
+let input = document.querySelector("#input");
+let button = document.querySelector("#btn");
 const container = document.querySelector("#container");
 
-const size = 16;
+let row_width = 480;
 
-for(let i=0; i < size; i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
 
-    for(let j=0; j < size; j++){
-        const square = document.createElement("div");
-        square.classList.add("square");
+button.addEventListener("click", ()=>{
+    let size = Number(input.value);
+    let squareHeight = row_width/size;
 
-        row.appendChild(square);
+    container.innerHTML = "";
+
+    for(let i=0; i < size; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.height = `${row_width/size}px`;
+        
+
+        for(let j=0; j < size; j++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+
+            square.style.height = `${squareHeight}px`;
+            square.style.width = `${squareHeight}px`;
+
+            row.appendChild(square);
+        }
+
+        container.appendChild(row);
     }
 
-    container.appendChild(row);
-}
-
-
+});
+ 
